32d3b42b578df0ca0d873d8fb38c0a8178807b2b
Luciano Santos <luciusjss@gmail.com>
2016-06-30 16:08:59 +0100

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherForecastApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weatherForecastApp
+ */
+angular.module('weatherForecastApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
