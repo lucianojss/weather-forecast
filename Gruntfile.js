88a5eb05b2133043fb321efae4c18350d313d45f
Luciano Santos <luciusjss@gmail.com>
2016-07-04 03:00:10 +0100

    fix fonts on grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -392,7 +392,14 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
-        }]
+        },
+	 {
+          expand: true,
+          cwd: 'bower_components/weather-icons',
+          src: 'font/*',
+          dest: '<%= yeoman.dist %>'
+        }	
+	]
       },
       styles: {
         expand: true,
